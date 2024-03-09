984a7691f0d7e8bd775d72d015714625c6bde6a1
Μιχαίας <mmcadoulos@gmail.com>
2024-03-08 22:10:45 -0600

    got validation to pass tests and work in browser

--- script.js
+++ script.js
@@ -3,8 +3,6 @@
 // const { validateInput, formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function () {
-  
-
   // let listedPlanets;
   // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
   // let listedPlanetsResponse;
@@ -18,23 +16,28 @@ window.addEventListener("load", function () {
   //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
   //   });
 
-    // task 2
-    let form = this.document.querySelector("form");
-    form.addEventListener("submit", () => {
-      let pilot = this.document.querySelector("input[name=pilotName]");
-      let copilot = this.document.querySelector("input[name=copilotName]");
-      let fuelLevel = this.document.querySelector("input[name=fuelLevel]");
-      let cargoLevel = this.document.querySelector("input[name=cargoMass]");
-      let documentVar = this.document;
-      let list = this.document.getElementById("faultyItems");
-      
-      formSubmission(documentVar, list, pilot, copilot, fuelLevel, cargoLevel);
-      
-      // if (!formSubmission(documentVar, list, pilot, copilot, fuelLevel, cargoLevel)){
-      //   this.alert("ALL forms must contain valid input");
-      // };
-      // alert("Submitted");
-    });
-
-
+  // task 2
+  let form = this.document.querySelector("form");
+  form.addEventListener("submit", () => {
+    let pilot = this.document.querySelector("input[name=pilotName]");
+    let copilot = this.document.querySelector("input[name=copilotName]");
+    let fuelLevel = this.document.querySelector("input[name=fuelLevel]");
+    let cargoLevel = this.document.querySelector("input[name=cargoMass]");
+    let documentVar = this.document;
+    let list = this.document.getElementById("faultyItems");
+    if (
+      validateInput(pilot.value) !== "Not a Number" ||
+      validateInput(copilot.value) !== "Not a Number" ||
+      validateInput(fuelLevel.value) !== "Is a Number" ||
+      validateInput(cargoLevel.value) !== "Is a Number"
+    ) {
+      event.preventDefault();
+      alert("all fields are required");
+      launchStatus.style.color = "red";
+      launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+    } else {
+      formSubmission(documentVar, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+      this.event.preventDefault();
+    }
+  });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -41,109 +41,35 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
   let copilotStatus = document.getElementById("copilotStatus");
   let fuelStatus = document.getElementById("fuelStatus");
   let cargoStatus = document.getElementById("cargoStatus");
+  
   list.style.visibility = "visible";
-  //if no preventDefault, first console.log doesn't show and visibility returns to hidden
-  // console.log("why wont this run?");
-  // console.log(validateInput(pilot));
+  pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+  copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+  fuelStatus.innerHTML = "Fuel level high enough for launch";
+  cargoStatus.innerHTML = "Cargo mass low enough for launch";
+  launchStatus.style.color = "green";
+  launchStatus.innerHTML = "Shuttle is Ready for Launch";
 
-
-
-//does this need to be at start?
-// pilotStatus.innerHTML = `Pilot ${pilot.value} is ready for launch`;
-//     copilotStatus.innerHTML = `Co-Pilot ${copilot.value} is ready for launch`;
-//     fuelStatus.innerHTML = "Fuel level high enough for launch";
-//     cargoStatus.innerHTML = "Cargo mass low enough for launch";
-//     launchStatus.style.color = "green";
-//     launchStatus.innerHTML = "Shuttle is Ready for Launch";
-
-  if (
-    validateInput(pilot.value) !== "Not a Number" ||
-    validateInput(copilot.value) !== "Not a Number" ||
-    validateInput(fuelLevel.value) !== "Is a Number" ||
-    validateInput(cargoLevel.value) !== "Is a Number" ||
-    Number(fuelLevel.value) < 10000 ||
-    Number(cargoLevel.value) > 10000
-  ) {
-    if (validateInput(pilot.value) !== "Not a Number") {
-      pilotStatus.innerHTML = `"Error: ${validateInput(pilot)}" is not ready`;
-    } else {
-      pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-    }
-    if (validateInput(copilot.value) !== "Not a Number") {
-      copilotStatus.innerHTML = `"Error: ${validateInput(
-        copilot
-      )}" is not ready`;
-    } else {
-      copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-    }
-    if (Number(fuelLevel) < 10000) {
-      fuelStatus.innerHTML = "Fuel level too low for launch";
-      // launchStatus.style.color = "red";
-      // launchStatus.innerHTML = "Shuttle not ready for launch";
-    } else {
-      fuelStatus.innerHTML = "Fuel level high enough for launch";
-    }
-    if (Number(cargoLevel) > 10000) {
-      cargoStatus.innerHTML =
-        "Cargo mass too heavy for launch";
-    } else {
-      cargoStatus.innerHTML = "Cargo mass low enough for launch";
-    }
+  if (validateInput(pilot) !== "Not a Number") {
+    pilotStatus.innerHTML = `"Error: ${validateInput(pilot)}" is not ready`;
     launchStatus.style.color = "red";
     launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-    // alert("ALL forms must contain valid input");
-    // event.preventDefault();
-  } else {
-
-  /*
-  if (
-    validateInput(pilot) !== "Not a Number" ||
-    validateInput(copilot) !== "Not a Number" ||
-    validateInput(fuelLevel) !== "Is a Number" ||
-    validateInput(cargoLevel) !== "Is a Number"
-  ) {
-    // if statements inside for individual text changes
-    if (validateInput(pilot) !== "Not a Number") {
-      pilotStatus.innerHTML = `"Error: ${validateInput(pilot)}" is not ready`;
-    }
-    if (validateInput(copilot) !== "Not a Number") {
-      copilotStatus.innerHTML = `"Error: ${validateInput(
-        copilot
-      )}" is not ready`;
-    }
-
-    alert("ALL forms must contain valid input");
-    launchStatus.style.color = "red";
-    launchStatus.innerHTML = "Shuttle not ready for launch";
-    event.preventDefault();
-  } else if (Number(fuelLevel.value) < 10000) {
-    fuelStatus.innerHTML = "Not enough fuel for the journey";
-    launchStatus.style.color = "red";
-    launchStatus.innerHTML = "Shuttle not ready for launch";
-    event.preventDefault();
-  } else if (Number(cargoLevel.value) > 10000) {
-    cargoStatus.innerHTML =
-      "there is too much mass for the shuttle to take off";
-    launchStatus.style.color = "red";
-    launchStatus.innerHTML = "Shuttle not ready for launch";
-    event.preventDefault();
-    
-  }*/
-    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-    fuelStatus.innerHTML = "Fuel level high enough for launch";
-    cargoStatus.innerHTML = "Cargo mass low enough for launch";
-    launchStatus.style.color = "green";
-    launchStatus.innerHTML = "Shuttle is Ready for Launch";
-    // event.preventDefault();
   }
-
-  // validate each individual input editing pilot and copilot list element
-  // check if fuel level is high enough editing the fuel li
-  // check if cargo mass is low enough editing the cargo li
-
-  // modify list based off of results?
-  //list style change to visible
+  if (validateInput(copilot) !== "Not a Number") {
+    copilotStatus.innerHTML = `"Error: ${validateInput(copilot)}" is not ready`;
+    launchStatus.style.color = "red";
+    launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+  }
+  if (Number(fuelLevel) < 10000) {
+    fuelStatus.innerHTML = "Fuel level too low for launch";
+    launchStatus.style.color = "red";
+    launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+  }
+  if (Number(cargoLevel) > 10000) {
+    cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+    launchStatus.style.color = "red";
+    launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+  }
 }
 
 async function myFetch() {
